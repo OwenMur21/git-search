ba9a1c75d2e0e3ee7db8045e6ccaa9df9e4665ce
Owenmur21 <owenmuriithi@gmail.com>
2018-08-17 22:36:33 +0300

    Updates elements

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  apiUrl: 'https://api.github.com/users/',
+  apikey: '?access_token=1e09e1baa573d7f49014ffe3db068ea7ce8475cd',
+  apiRepokey: '/repos?access_token=1e09e1baa573d7f49014ffe3db068ea7ce8475cd',
 };
