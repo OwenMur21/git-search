e177a14e2866011375aeb322391415bd00bdd7d4
Owenmur21 <owenmuriithi@gmail.com>
2018-08-20 20:45:33 +0300

    Adds directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,26 @@ import { ProfileComponent } from './profile/profile.component';
 import {ProfileService} from './profiles/profile.service';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
 import {FormsModule} from '@angular/forms';
-import { RepositoriesComponent } from './repositories/repositories.component';
-import { RouterModule, Routes } from '@angular/router';
+import { HighlightDirective } from './highlight.directive';
+// import { RepositoriesComponent } from './repositories/repositories.component';
+// import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {path: 'profile', component: ProfileComponent},
-  {path: 'repositories', component: RepositoriesComponent}
-];
+// const routes: Routes = [
+//   {path: 'profile', component: ProfileComponent},
+//   {path: 'repositories', component: RepositoriesComponent}
+// ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     ProfileFormComponent,
-    RepositoriesComponent
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot(routes)
+    FormsModule
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
